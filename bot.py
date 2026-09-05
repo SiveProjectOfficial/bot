c19184bdfeeb5c886fad9a80c84219fab3e212ec
Lalala <1214yt.na722@gmail.com>
2026-09-06 00:56:51 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,6 +38,9 @@ def is_safe(text, ng_words):
     place_pattern = r'(北海道|青森県|岩手県|宮城県|秋田県|山形県|福島県|茨城県|栃木県|群馬県|埼玉県|千葉県|東京都|神奈川県|新潟県|富山県|石川県|福井県|山梨県|長野県|岐阜県|静岡県|愛知県|三重県|滋賀県|京都府|大阪府|兵庫県|奈良県|和歌山県|鳥取県|島根県|岡山県|広島県|山口県|徳島県|香川県|愛媛県|高知県|福岡県|佐賀県|長崎県|熊本県|大分県|宮崎県|鹿児島県|沖縄|ヒロシマ|ナガサキ|広島|長崎|沖縄県|[都道府県]|.+[市区町村])'
     if re.search(place_pattern, text):
         return False
+    place_pattern = r'(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z)'
+    if re.search(place_pattern, text):
+        return False
 
     # NGワードチェック（テキスト内にNGワードが含まれているか完全一致・部分一致でチェック）
     for word in ng_words:
